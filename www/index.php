4e5d84dba6f65016fd96089502ae076dd80b456e
Bryan Green <bcgreen@gmail.com>
2013-05-03 10:42:26 -0700

    debugging stuff

--- www/index.php
+++ www/index.php
@@ -15,7 +15,6 @@
  * Root directory of Drupal installation.
  */
 define('DRUPAL_ROOT', getcwd());
-die("died");
 require_once DRUPAL_ROOT . '/includes/bootstrap.inc';
 drupal_bootstrap(DRUPAL_BOOTSTRAP_FULL);
 menu_execute_active_handler();
